417a12b952e5fde945108c1166c4a116efcf22ad
unknown <hassanmharajly@hotmail.com>
2021-09-03 14:56:48 -0400

    extracting to component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import DogFacts from "./DogFacts"
+import {Component} from "react";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <DogFacts/>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>
 
-        <span>Dog Facts: random fact</span>
       </header>
     </div>
   );
---
+++ src/DogFacts.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class DogFacts extends React.Component {
+    render() {
+        return (
+            <span>Dog Facts: random fact</span>
+        );
+    }
+}
